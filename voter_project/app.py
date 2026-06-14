2320220eb3b9842c542faadafc367d9a9f01742f
Aryan Bhandari <aryanbhandari669@gmail.com>
2026-06-14 18:40:47 +0530

    Update app.py

--- voter_project/app.py
+++ voter_project/app.py
@@ -28,19 +28,18 @@ def handle_voters():
     voters = load_voters()
     if request.method == 'POST':
         new_voter = request.json
-        # Duplicate check
         if any(v['id'] == int(new_voter['id']) for v in voters):
             return jsonify({"message": "Duplicate ID!"}), 400
         
         voters.append({"id": int(new_voter['id']), "name": new_voter['name']})
         save_voters(voters)
         return jsonify({"message": "Registered!"})
-    
-    # If sorting is requested
+
     if request.args.get('sort') == 'true':
         voters.sort(key=lambda x: x['id'])
     
     return jsonify(voters)
 
 if __name__ == '__main__':
+
     app.run(debug=True, port=5000)
